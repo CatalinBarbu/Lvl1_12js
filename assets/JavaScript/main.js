30495a7787953bf721a085eb665cccce97535d2f
CatalinBarbu <121017880+CatalinBarbu@users.noreply.github.com>
2023-01-03 12:58:46 +0100

    First Commit

---
+++ assets/JavaScript/main.js
@@ -0,0 +1,20 @@
+let score = 5 + 5 * 10;
+console.log("Ergebnis: " + score);
+
+let scoreE = (5 + 5) * 10;
+console.log("Ergebnis: " + scoreE);
+
+let number = 0;
+/* number = number + 10; */
+number += 10;
+console.log("Ergebnis:" + number);
+
+let zahl = 1;
+zahl = zahl + 1;
+zahl += 1;
+
+zahl++;
+console.log("Increment: " + zahl);
+
+zahl--;
+console.log("Dekrement: " + zahl);
